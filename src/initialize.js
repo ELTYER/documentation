e9cd0f8b458b004f00c4fba43a609ec2962a000a
marc <marcvanduyn1@gmail.com>
2022-01-06 21:57:37 +0100

    fix(SIDENAV): fix sidenav open rendering

--- src/initialize.js
+++ src/initialize.js
@@ -1,12 +1,15 @@
 import {sideNavOpenAction} from "./redux/actions";
 
 export async function initialize(req, store) {
-    const UA = req.headers['user-agent'];
-    const isMobile = Boolean(UA.match(
-        /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
-    ))
 
-    if(isMobile) {
-        store.dispatch(sideNavOpenAction(false));
+    if(req!== null && req !== undefined) {
+        const UA = req.headers['user-agent'];
+        const isMobile = Boolean(UA.match(
+            /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+        ))
+
+        if(isMobile) {
+            store.dispatch(sideNavOpenAction(false));
+        }
     }
 }
